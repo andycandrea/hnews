a2e10f33f00e7493d275433fffe1f42393f87c00
Andy Andrea <andy.andrea@viget.com>
2014-06-18 10:28:41 -0400

    Fixed issues based on review

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,4 @@
 Rails.application.routes.draw do
-  get 'articles/index'
-
   root to: 'articles#index'
-
   resources :articles, only: [:new, :create, :index]
 end
--- spec/features/viewing_articles_spec.rb
+++ spec/features/viewing_articles_spec.rb
@@ -3,26 +3,24 @@ require 'spec_helper'
 describe "viewing articles" do
   let!(:old_article) { create(:article) }
   let!(:new_article) { create(:article, title: "Title 2") }
+  
+  before { visit '/articles' }
 
   it "should display new entry" do
-    visit '/articles'
     expect(page).to have_content('Such title')
     expect(page).to have_content('http://www.much.url')
     expect(page).to have_content('Wow.')
   end
   
   it "should have the content 'Title'" do
-    visit '/articles'
     expect(page).to have_content('Title')
   end
 
   it "should have the content 'Created At'" do
-    visit '/articles'
     expect(page).to have_content('Created At')
   end
 
   it "should be sorted by descending time of creation" do
-    visit '/articles'
     page.body.index(new_article.title).should < page.body.index(old_article.title)
   end
 end
