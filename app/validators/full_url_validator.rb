34e0a3c411fad46361108f17964f3a4030ccb2cb
Andy Andrea <andy.andrea@viget.com>
2014-06-19 16:59:16 -0400

    Modified regex

--- app/validators/full_url_validator.rb
+++ app/validators/full_url_validator.rb
@@ -4,7 +4,7 @@ class FullUrlValidator < ActiveModel::EachValidator
   
   def validate_each(record, attribute, value)
     #Not a wonderful regex, but allows for URLs that don't include scheme
-    url_regex = /^[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,6}(:[0-9]{1,5})?(\/.*)?$/
+    url_regex = /^[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,}(:[0-9]{1,5})?(\/.*)?$/
     
     unless (valid_full_url?(value) || url_regex =~ value || value == "")
       record.errors[attribute] << (options[:message] || 'is not a valid URL.')
