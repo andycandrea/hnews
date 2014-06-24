c9976d87f7ea5baef561272ba6e59bdc770f66c1
Andy Andrea <andy.andrea@viget.com>
2014-06-24 12:54:43 -0400

    Downcase email prior to saving to db

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ActiveRecord::Base
+  before_save { self.email = email.downcase if email? }
+  
   #Temporary regex - will eventually validate by sending an email
   EMAIL_REGEX = /\A[\x00-\x1F!\#\$%&\'\*\+-\/0-9=\?A-Z\x5E-\x7F]+@(([a-zA-Z][a-zA-Z0-9]+)|(([a-zA-Z]([a-zA-Z0-9])+\.)+([a-zA-Z]([a-zA-Z0-9])+)))\Z/
 
