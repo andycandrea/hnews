33fcde6919c11a99daa380899380587461e7ffc2
Andy Andrea <andy.andrea@viget.com>
2014-06-27 11:21:12 -0400

    removed comment

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,5 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-#  include SessionsHelper
   
   def sign_in(user)
     cookies.permanent[:remember_token] = user.generate_remember_token
