7dad0447fc1bf9eb617de35238f5d46be822b021
Andy Andrea <andy.andrea@viget.com>
2014-07-02 16:11:57 -0400

    Added new test

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -39,5 +39,5 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :test
   config.action_mailer.perform_deliveries = true
   config.action_mailer.deliveries = []
-  config.action_mailer.default_url_options = { host: 'localhost:3000' }
+  config.action_mailer.default_url_options = { host: 'www.example.com' }
 end
