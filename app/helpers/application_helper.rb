96cb78df49c4d57df3529f7d0e32a81862653a87
Andy Andrea <andy.andrea@viget.com>
2014-07-11 17:21:47 -0400

    Change date format to 'x time ago'

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,8 @@
 module ApplicationHelper
+  def format_date(date)
+    "#{distance_of_time_in_words(date, Time.now, options = {})} ago"
+  end
+  
   def url_for_article(article)
     article.url.presence || article_path(article)
   end
