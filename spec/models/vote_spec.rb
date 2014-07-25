456d6582ca47f3e0b9807f05a26ce8dd9a6a2f1a
Andy Andrea <andy.andrea@viget.com>
2014-07-25 15:52:52 -0400

    Add additional validation to vote

--- spec/models/vote_spec.rb
+++ spec/models/vote_spec.rb
@@ -6,4 +6,11 @@ describe Vote do
 
   it { should belong_to(:user) }
   it { should belong_to(:votable) }
+
+  it 'does not allow a User to vote multiple times per votable' do
+    user = create(:user)
+    article = create(:article, :has_url)
+    create(:vote, user: user, votable: article)
+    build(:vote, user: user, votable: article).save.should == false
+  end
 end
